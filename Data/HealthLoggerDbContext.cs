82240026ecd5a140d2ccb917d23b91f86ef67cef
Rahul Pandey <rahulpandey862@gmail.com>
2021-05-28 08:23:52 +0000

    Added new class library for Data Models. Added HealthLoggerDbContext.cs file

---
+++ Data/HealthLoggerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Data
+{
+    public class HealthLoggerDbContext : DbContext
+    {
+
+        public HealthLoggerDbContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
