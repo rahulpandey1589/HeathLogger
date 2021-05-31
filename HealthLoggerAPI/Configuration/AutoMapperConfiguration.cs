93c59698a942da4c97d48c2b238fdc10344bfc25
Rahul Pandey <rahulpandey862@gmail.com>
2021-05-31 13:04:42 +0000

    Added base mapper file

---
+++ HealthLoggerAPI/Configuration/AutoMapperConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace HealthLoggerAPI.Configuration
+{
+    public static class AutoMapperConfiguration
+    {
+        public static void AddAutoMapper(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetAssembly(typeof(Business.ConcreteImplementation.Mappers.BaseMapper)));
+        }
+
+    }
+}
